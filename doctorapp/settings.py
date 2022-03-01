4f5c7b0357805723a123febb01bf0bc5b2d2c354
Manokamna Singh <85088233+Manokamnasingh1@users.noreply.github.com>
2022-03-01 10:29:47 +0530

    Update settings.py

--- doctorapp/settings.py
+++ doctorapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i5rr7llgw6twupd(^@^y63n)35)@57q7xf2c)-&_dor=64b(ue
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
