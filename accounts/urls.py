d3ddb0fe0374fb549f020400bbf20cd71cf80b54
Manokamna <manokamnasingh2001@example.com>
2022-03-01 10:16:30 +0530

    second commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path,include
-
+from django.conf import settings
 from accounts import views
-
+from django.conf.urls.static import static
 urlpatterns = [
 
     path('', views.accounts, name ='accounts'),
     path('order', views.order, name ='order'),
     
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
